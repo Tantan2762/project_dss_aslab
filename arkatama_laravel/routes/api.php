c3a8e128627267e7f46ff7c5c56f40077b2118ba
Tantan2762 <tantan.nugraha@upi.edu>
2023-06-05 21:05:41 +0700

    [FIX]Menambah Route CRUD API

--- arkatama_laravel/routes/api.php
+++ arkatama_laravel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApiDprodukController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/dproduk', [ApiDprodukController::class, 'index']);
+Route::post('/dproduk/store', [ApiDprodukController::class, 'store']);
+Route::put('/dproduk/update', [ApiDprodukController::class, 'update']);
+Route::delete('/dproduk/delete/{id}', [ApiDprodukController::class, 'delete']);
